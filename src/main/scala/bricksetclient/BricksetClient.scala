786706a84d55b262a334962b8988e8956d81061f
Gabor Pihaj <gabor.pihaj@gmail.com>
2015-03-29 20:58:42 +0200

    Implement checkKey in BricksetClient class

--- src/main/scala/bricksetclient/BricksetClient.scala
+++ src/main/scala/bricksetclient/BricksetClient.scala
@@ -1,7 +1,40 @@
 package bricksetclient
 
+import bricksetproduceractor._
+import scala.collection.JavaConversions._
+
+import akka.actor.{ActorSystem, Props}
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
 class BricksetClient(val apiKey: String) {
-  val endPoint = "http://brickset.com/webservices/brickset.asmx"
+  // create an actor system
+  private val sys = ActorSystem("BricksetClient")
+
+  // instantiate a BricksetProducerActor actor
+  private val actor = sys.actorOf(Props[BricksetProducerActor], "bricksetproduceractor")
+
+  // set promise timmeouts
+  implicit val timeout = Timeout(60 seconds)
+
+  /**
+   * Builds a BricksetRequest, and automatically adds the apikey param
+   */
+  private def buildRequest(operation: String, params: List[String]):  BricksetRequest = {
+    new BricksetRequest(apiKey :: params, Map(
+        "operationName" -> operation
+      ))
+  }
+
+  /**
+   * Checks the current api key
+   */
+  def checkKey(): Future[String] = {
+    (actor ? buildRequest("checkKey", List())).mapTo[String]
+  }
 }
 
 
--- src/test/scala/TestBriscketClient.scala
+++ src/test/scala/TestBriscketClient.scala
@@ -3,13 +3,23 @@ package bricksetapispec
 import org.scalatest._
 import bricksetclient._
 
-class BricksetClientSpec extends FlatSpec with Matchers {
-  it should "set default properties" in {
-    var apiKey = "dummy-key"
-    val client = new BricksetClient(apiKey)
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import akka.util.Timeout
 
-    client shouldBe a [BricksetClient]
-    client.apiKey shouldBe (apiKey)
-    client.endPoint shouldBe ("http://brickset.com/webservices/brickset.asmx")
+class BricksetClientSpec extends FlatSpec with Matchers {
+  val source = io.Source.fromFile("src/test/resources/api.key")
+  val apikey = source.mkString.trim
+  source.close
+
+  val duration = (60 seconds)
+
+  it should "check the api key" in {
+    val client = new BricksetClient(apikey)
+    
+    val future = client.checkKey()
+    val result = Await.result(future, duration)
+
+    result should fullyMatch regex "OK \\(v2(ACM|)\\)"
   }
 }
