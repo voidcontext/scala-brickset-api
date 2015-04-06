c81a1b65e6086c1a8f6c5d071e70c9eb27adf992
Gabor Pihaj <gabor.pihaj@gmail.com>
2015-04-07 00:19:55 +0200

    Modify settings to publish brickset client to sonatype repos

--- project/Build.scala
+++ project/Build.scala
@@ -2,12 +2,47 @@ package build
 
 import sbt._
 import Keys._
+import xerial.sbt.Sonatype.SonatypeKeys._
+import xerial.sbt.Sonatype.sonatypeSettings
 
 object BricksetClientBuild extends Build {
+  val publishSettings = Seq(
+    profileName := "voidcontext",
+    publishMavenStyle := true,
+    publishTo := {
+      val nexus = "https://oss.sonatype.org/"
+      if (isSnapshot.value)
+        Some("snapshots" at nexus + "content/repositories/snapshots") 
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    pomExtra := {
+      <url>https://github.com/voidcontext/scala-brickset-client</url>
+      <licenses>
+        <license>
+          <name>MIT</name>
+          <url>http://opensource.org/licenses/MIT</url>
+        </license>
+      </licenses>
+      <scm>
+        <connection>scm:git:github.com/voidcontext/scala-brickset-client</connection>
+        <developerConnection>scm:git:git@github.com:voidcontext/scala-brickset-client</developerConnection>
+        <url>github.com/voidcontext/scala-brickset-client</url>
+      </scm>
+      <developers>
+        <developer>
+          <id>voidcontext</id>
+          <name>Gabor Pihaj</name>
+          <url>http://voidcontext.github.io</url>
+        </developer>
+      </developers>
+    }
+  )
+
   lazy val root = Project(
     "brickset-client",
     file("."),
-    settings = Defaults.defaultSettings ++ Seq(
+    settings = Defaults.defaultSettings ++ sonatypeSettings ++ publishSettings ++ Seq(
       name := "bricksetclient",
       organization := "io.github.voidcontext",
       version := "0.0.1-SNAPSHOT",
